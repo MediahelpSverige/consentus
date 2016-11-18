8e17b242759869efd2c9bb40cebbffcd34b17504
alienwizard <johanwendin@gmail.com>
2016-11-18 10:48:42 +0100

    db

--- wp-config.env.php
+++ wp-config.env.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Setup environments
- * 
+ *
  * Set environment based on the current server hostname, this is stored
  * in the $hostname variable
- * 
- * You can define the current environment via: 
+ *
+ * You can define the current environment via:
  *     define('WP_ENV', 'production');
- * 
+ *
  * @package    Studio 24 WordPress Multi-Environment Config
  * @version    1.0
  * @author     Studio 24 Ltd  <info@studio24.net>
@@ -27,13 +27,16 @@
 switch ($hostname) {
     case 'localhost:8080':
         define('WP_ENV', 'development');
-		break;																		
+		break;
 
     case 'consentus.mediahelpcrm.se':
         define('WP_ENV', 'staging');
         break;
 
+    case 'consentus.mediahelpcrm.se':
+      define('WP_ENV', 'staging');
+      break;
     case 'www.domain.com':
-    default: 
+    default:
         define('WP_ENV', 'production');
 }
