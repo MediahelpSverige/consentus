72a059704399337566806fc1acbac5e514492bab
viktorkattstrom <viktorkattstrom@hotmail.com>
2017-01-31 11:04:47 +0100

    Changed some images and texts

--- wp-content/themes/consentus/page-tempaltes/produkter.php
+++ wp-content/themes/consentus/page-tempaltes/produkter.php
@@ -41,11 +41,16 @@ get_header();
 		?>
 
 		<div class="row">
-			<div class="product-wrapper">
-				<?php 
-				$the_query = new WP_Query(array('post_type' => 'produkt', 'post_per_page' => -1, 'order' => 'ASC'));
-				if ( $the_query->have_posts() ) : 
-					while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+			<?php 
+			$the_query = new WP_Query(array('post_type' => 'produkt', 'cat' => 3, 'post_per_page' => -1, 'order' => 'ASC'));
+			if ( $the_query->have_posts() ) : ?>
+
+				<h2>
+					Eldningsolja
+				</h2>
+
+				<div class="product-wrapper">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 						
 						<article class="col-sm-4 col-xs-6 product-container">
 							<h3>
@@ -57,9 +62,109 @@ get_header();
 
 						<?php 
 					endwhile; 
-					wp_reset_postdata();
-				endif; ?>				
-			</div>
+					wp_reset_postdata(); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php 
+			$the_query = new WP_Query(array('post_type' => 'produkt', 'cat' => 4, 'post_per_page' => -1, 'order' => 'ASC'));
+			if ( $the_query->have_posts() ) : ?>
+
+				<h2>
+					Diesel
+				</h2>
+
+				<div class="product-wrapper">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						
+						<article class="col-sm-4 col-xs-6 product-container">
+							<h3>
+								<?php the_title(); ?>
+							</h3>
+
+							<?php the_content(); ?>
+						</article>
+
+						<?php 
+					endwhile; 
+					wp_reset_postdata(); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php 
+			$the_query = new WP_Query(array('post_type' => 'produkt', 'cat' => 5, 'post_per_page' => -1, 'order' => 'ASC'));
+			if ( $the_query->have_posts() ) : ?>
+
+				<h2>
+					Bunker
+				</h2>
+
+				<div class="product-wrapper">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						
+						<article class="col-sm-4 col-xs-6 product-container">
+							<h3>
+								<?php the_title(); ?>
+							</h3>
+
+							<?php the_content(); ?>
+						</article>
+
+						<?php 
+					endwhile; 
+					wp_reset_postdata(); ?>
+				</div>
+			<?php endif; ?>	
+
+			<?php 
+			$the_query = new WP_Query(array('post_type' => 'produkt', 'cat' => 6, 'post_per_page' => -1, 'order' => 'ASC'));
+			if ( $the_query->have_posts() ) : ?>
+
+				<h2>
+					Smörjmedel
+				</h2>
+
+				<div class="product-wrapper">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						
+						<article class="col-sm-4 col-xs-6 product-container">
+							<h3>
+								<?php the_title(); ?>
+							</h3>
+
+							<?php the_content(); ?>
+						</article>
+
+						<?php 
+					endwhile; 
+					wp_reset_postdata(); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php 
+			$the_query = new WP_Query(array('post_type' => 'produkt', 'cat' => 7, 'post_per_page' => -1, 'order' => 'ASC'));
+			if ( $the_query->have_posts() ) : ?>
+
+				<h2>
+					Hyrtankar
+				</h2>
+
+				<div class="product-wrapper">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						
+						<article class="col-sm-4 col-xs-6 product-container">
+							<h3>
+								<?php the_title(); ?>
+							</h3>
+
+							<?php the_content(); ?>
+						</article>
+
+						<?php 
+					endwhile; 
+					wp_reset_postdata(); ?>
+				</div>
+			<?php endif; ?>									
 		</div>
 	</div>
 	</div>
